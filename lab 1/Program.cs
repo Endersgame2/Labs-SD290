5bbd4b6a28fd922b520ca68cb66fc1c742a16d67
Sushant Sharma <sushantsharmapb13@gmail.com>
2022-06-15 23:44:37 -0400

    Just written the main idea of the code

--- lab 1/Program.cs
+++ lab 1/Program.cs
@@ -1,11 +1,4 @@
-﻿// Palandrome Lab 3
-// Here our simple mindset is to take a user input string and check if it is a palandrome
-// I will simply take a string as user input and to check each character I will convert it to char
-// Then I need to reverse that char arr So I will use reverse
-// Now its reversed string So I will convert it back to string 
-// last and if condition to check if they are palandrome
-
-using System;
+﻿
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,24 +10,19 @@ namespace lab_1
     {
         static void Main(string[] args)
         {
+            // Lab 6
 
-            Console.WriteLine("Please Enter a String to check palandrome");
-            string userInput = Console.ReadLine();
-            char[] inputToChar = userInput.ToCharArray();
+            // There will only be one celebrity as lets suppose we have 3 people where we have
+            // That is easy and simple that no one is celebrity.
+            // a , b, c ,d
+            // and both a ,b and d knows c but c dont know any of them thats poosible.
+            // but if we say b , d knows a and c but they dont know b and d.
+            //  lets say in universe of a b d there is c who they know but c dont know them
+            // if we say b says im celeberity and c dont know him thats why he can t be celebrity.
+            // Thats the reason for no two celebrities at one time.
 
-            // I took array.reverse from stackOverflow as a refrence
-            Array.Reverse(inputToChar);
-            string reversedArr = new string(inputToChar);
+            // code
 
-
-            if (userInput.Equals(reversedArr))
-            {
-                Console.WriteLine("Its a Plandrome!!! Yeah");
-            }
-            else
-            {
-                Console.WriteLine("Oof !! its not a palandrome");
-            }
 
         }
     }
