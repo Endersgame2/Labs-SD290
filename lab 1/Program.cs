bf5652c138b68a919e73c091c0867ce6d9223351
Sushant Sharma <sushantsharmapb13@gmail.com>
2022-06-15 23:41:27 -0400

    Completed Lab 4

--- lab 1/Program.cs
+++ lab 1/Program.cs
@@ -10,34 +10,50 @@ namespace lab_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please Enter an integer");
-            int n = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"Please enter {n} Words");
-            string[] input = new string[n];
-            for (int i = 0; i < input.Length; i++)
-            {
-                input[i] = Console.ReadLine();
-            }
-            char[] arr = new char[input.Length];
-            Console.WriteLine("Please Enter a character");
+            // lab 4
 
-            char ch = Console.ReadKey().KeyChar;
-            int count = 0;
-            int perecentage = 0;
+            // part 1
+            // we simply need to use List.max as given
 
-            for (int i = 0; i < arr.Length; i++)
+            var list1 = new List<int> { 1, 5, 3 };
+            var list2 = new List<int> { 9, 7, 3, -2 };
+            var list3 = new List<int> { 5, 9, 2 };
+
+            Console.WriteLine($"List 1 has maximum of {list1.Max()}. List 2 has a maximum of {list2.Max()} List 3 has a maximum of {list3.Max()}");
+
+            // part 2
+            // Highest Grades
+
+            var sub1 = new List<int> { 85, 92, 67, 94, 94 };
+            var sub2 = new List<int> { 50, 60, 57, 95 };
+            var sub3 = new List<int> { 95 };
+
+            var grades = new List<int> { sub1.Max(), sub2.Max(), sub3.Max() };
+            Console.WriteLine($"The highest grade is {grades.Max()}.");
+
+            //part 3
+            // Simple Bubble sort
+            // refrence stack overflow and youtube
+
+            int[] arr = { 6, -2, 5, 3 };
+            int temp;
+            for (int i = 0; i <= arr.Length - 2; i++)
             {
-                if (ch == arr[i])
-                { 
-                    count++;
-                }else
+                for (int j = 0; j <= arr.Length - 2; j++)
                 {
-                    perecentage++;
+                    if (arr[i] > arr[i + 1])
+                    {
+                        temp = arr[i + 1];
+                        arr[i + 1] = arr[i];
+                        arr[i] = temp;
+                    }
                 }
             }
 
-            Console.WriteLine($" The Letter '{ch}' appeared {count} times in the array. This letter make up {perecentage} of the total number of Characters.");
-
+            foreach (var i in arr)
+            {
+                Console.Write(i + ", ");
+            }
         }
     }
 }
